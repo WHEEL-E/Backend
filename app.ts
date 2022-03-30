852c0431e5c1cb75d13898fae34d0adc9e8f1968
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2022-03-31 00:32:17 +0200

    Basic Projetc Structure

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express, { Application } from "express";
+const app: Application = express();
+const port: number = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
