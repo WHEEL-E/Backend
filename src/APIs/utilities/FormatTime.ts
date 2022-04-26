5f061f0b7316669198d2c01a59fd5fa19776b97f
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2022-04-27 01:19:38 +0200

    fix merging issues-2

--- src/APIs/utilities/FormatTime.ts
+++ src/APIs/utilities/FormatTime.ts
@@ -2,6 +2,7 @@ const getTimeNowFormatted = () => {
   const nowDate = new Date()
   let nowDateFormatted = `${nowDate.getDay()}-${nowDate.getMonth() + 1}-`
   nowDateFormatted = ` ${nowDate.getFullYear()} ${nowDate.getHours()}:${nowDate.getMinutes()}:${nowDate.getSeconds()}`
+
   return nowDateFormatted
 }
 
