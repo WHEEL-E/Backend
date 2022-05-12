8c0265077aed98cce0b978e9d3c14a7cbb3a72ac
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2022-05-13 01:38:02 +0200

    Invitation Schema

---
+++ src/APIs/schema/Invitation.schems.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, model } from 'mongoose'
+
+const InvitationSchema = new Schema(
+  {
+    form_id: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Patient',
+      required: true
+    },
+    to_id: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Supervisior',
+      required: true
+    },
+    status: {
+      type: String,
+      values: ['Pending', 'Accepted', 'Rejected'],
+      default: 'Pending'
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  }
+)
+
+export default model('Invitations', InvitationSchema)
