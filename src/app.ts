1e5d4fcc74314227e5fa53e484300449ad434cf8
Alaa <leoelkzaz@yahoo.com>
2022-07-14 21:42:55 +0200

    changing localhost and port

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,10 @@ import {
 import express, { Application } from 'express'
 import Routes from './APIs/routes/index'
 import cors from 'cors'
-import numeral from 'numeral'
 
 const app: Application = express()
-const port = 3000
-const host = 'localhost'
+const port = 8080
+const host = '127.0.0.1'
 
 export const database = require('./APIs/config/database_config')()
 
@@ -23,8 +22,3 @@ app.use(notFoundHandler)
 app.listen(port, () => {
   return console.log(`Express is listening at http://${host}:${port}`)
 })
-
-setInterval(() => {
-  const { rss } = process.memoryUsage()
-  console.log(numeral(rss).format('0.0 ib'))
-}, 5000)
