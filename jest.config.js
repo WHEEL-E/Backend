0c79b0c494d3617fee67e3609c3da72a6ba97b5f
Alaa <leoelkzaz@yahoo.com>
2022-07-15 00:08:09 +0200

    remove tests

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-exports.__esModule = true
-exports.default = {
-  preset: 'ts-jest',
-  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node', 'd.ts'],
-  testMatch: ['**/*.test.ts'],
-  verbose: true,
-  testEnvironment: 'node'
-}
